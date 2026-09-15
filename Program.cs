204d8a246442fffc8cdf4b924a4036ed270e84b1
NewtonZhan <zhanyunze0601@gmail.com>
2026-09-15 13:15:16 +0800

    feat(program): Add main function

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BookManagementApplication
+{
+    class BookManagement
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Welcome to Book Management System");
+        }
+    }
+}
